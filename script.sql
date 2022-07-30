4dd8752c6eb4a52e9f11ba4d8a0e77808aea9d9e
Gabriel Henrique Dev <59948825+gab3-dev@users.noreply.github.com>
2022-07-29 21:40:34 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -8,3 +8,21 @@ create table Clientes(
   CPFCliente int not null
 );
 
+create table Vendas(
+  IDVenda int not null auto_increment primary key,
+  DataVenda date not null,
+  IDCliente int not null,
+  foreign key (IDCliente) references Clientes(IDCliente)
+);
+
+create table Produto(
+  IDProduto int not null auto_increment primary key,
+  NomeProduto varchar(50) not null,
+  PrecoUnitario double not null
+);
+
+create table ItensVendidos(
+  IDVenda int not null,
+  IDProduto int not null,
+  QtdeVendida int not null
+);
