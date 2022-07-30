6c872d5cd55ac8d0908af7c3d83048c233c4f3a3
Gabriel Henrique Dev <59948825+gab3-dev@users.noreply.github.com>
2022-07-29 21:30:03 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,1 +1,10 @@
 create database if not exists revisao;
+
+use revisao;
+
+create table Clientes(
+  IDCliente int not null auto_increment primary key,
+  NomeCliente varchar(50) not null,
+  CPFCliente int not null
+);
+
